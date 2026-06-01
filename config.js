c4f163b564cd9b2499413b66ea6d2bef79d3b3da
renatojacob-crypto <renato.jacob@yxzrobotica.com.br>
2026-06-01 19:30:27 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const URL_GAS = "https://script.google.com/macros/s/AKfycbxwiN-CL2N_rwlANqJQS-Y48_W7Y6yllq09wl3-qQc-vEMRZqU7nQFPv9UXx9xHCeYAGQ/exec";
+const URL_GAS = "https://script.google.com/macros/s/AKfycbz4pstnXqO5ezjYVbVG89MrRJSVa5yNsN3eKIqoqYWiV1heBZZxfJheuy0kOd8k9HtN4Q/exec";
